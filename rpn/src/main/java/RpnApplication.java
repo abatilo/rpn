89bee1c0dec43029c992c2f8c4af585d81fbd5c4
Aaron <AaronBatilo@gmail.com>
2018-01-20 18:22:01 -0700

    Evaluate single level expressions
    As long as you don't have nested expressions, we can evaluate them now

--- rpn/src/main/java/RpnApplication.java
+++ rpn/src/main/java/RpnApplication.java
@@ -1,5 +1,52 @@
+import java.util.stream.Stream;
+
 public class RpnApplication {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    System.out.println(eval("+ 2 3"));
+    System.out.println(eval("- 2 3"));
+    System.out.println(eval("/ 2 3"));
+    System.out.println(eval("* 2 3"));
+  }
+
+  public static float eval(String s) {
+    String[] tokens = s.split(" ");
+    if (tokens.length != 3) {
+      throw new IllegalArgumentException("Unparseable evaluation string");
+    }
+    String op = tokens[0];
+    if (!(op.equals("+") ||
+          op.equals("-") ||
+          op.equals("/") ||
+          op.equals("*"))) {
+      throw new IllegalArgumentException("Operand is not valid");
+    }
+
+    Float leftOperand = 0.0f;
+    try {
+      leftOperand = Float.valueOf(tokens[1]);
+    } catch (NumberFormatException e) {
+      throw new IllegalArgumentException("First operand is not a valid float");
+    }
+
+    Float rightOperand = 0.0f;
+    try {
+      rightOperand = Float.valueOf(tokens[2]);
+    } catch (NumberFormatException e) {
+      throw new IllegalArgumentException("Second operand is not a valid " +
+          "float");
+    }
+
+    switch (op) {
+      case "+":
+        return leftOperand + rightOperand;
+      case "-":
+        return leftOperand - rightOperand;
+      case "/":
+        return leftOperand / rightOperand;
+      case "*":
+        return leftOperand * rightOperand;
+      default:
+        return 0.0f;
+    }
   }
 }
