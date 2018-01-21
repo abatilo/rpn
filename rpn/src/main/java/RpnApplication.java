45be898554e84609134a4d066bd4b827423d9e0a
Aaron <AaronBatilo@gmail.com>
2018-01-20 18:55:06 -0700

    Implement RPN calculator
    This was a lot easier than I thought it was going to be.

--- rpn/src/main/java/RpnApplication.java
+++ rpn/src/main/java/RpnApplication.java
@@ -1,24 +1,41 @@
-import java.util.stream.Stream;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
 
 public class RpnApplication {
   public static void main(String[] args) {
-    System.out.println(eval("2 3 +"));
-    System.out.println(eval("2 3 -"));
-    System.out.println(eval("2 3 /"));
-    System.out.println(eval("2 3 *"));
+    System.out.println(eval("1 1 1 + 7 * +"));
   }
 
-  public static float eval(String s) {
+  public static String eval(String s) {
+    StringTokenizer tokenizer = new StringTokenizer(s);
+    ArrayDeque<String> stack = new ArrayDeque<>();
+    while (tokenizer.hasMoreTokens()) {
+      String token = tokenizer.nextToken();
+      if (isOperator(token)) {
+        String rightOperand = stack.pop();
+        String leftOperand = stack.pop();
+        stack.push(String.valueOf(evalSingle(leftOperand + " " + rightOperand + " " + token)));
+      } else {
+        stack.push(token);
+      }
+    }
+    if (stack.size() != 1) {
+      throw new IllegalArgumentException("Invalid expression");
+    }
+    return stack.pop();
+  }
+
+  private static boolean isOperator(String op) {
+    return (op.equals("+") ||
+          op.equals("-") ||
+          op.equals("/") ||
+          op.equals("*"));
+  }
+
+  private static float evalSingle(String s) {
     String[] tokens = s.split(" ");
     if (tokens.length != 3) {
       throw new IllegalArgumentException("Unparseable evaluation string");
-    }
-    String op = tokens[2];
-    if (!(op.equals("+") ||
-          op.equals("-") ||
-          op.equals("/") ||
-          op.equals("*"))) {
-      throw new IllegalArgumentException("Operand is not valid");
     }
 
     Float leftOperand = 0.0f;
@@ -34,6 +51,14 @@ public class RpnApplication {
     } catch (NumberFormatException e) {
       throw new IllegalArgumentException("Second operand is not a valid " +
           "float");
+    }
+
+    String op = tokens[2];
+    if (!(op.equals("+") ||
+          op.equals("-") ||
+          op.equals("/") ||
+          op.equals("*"))) {
+      throw new IllegalArgumentException("Operand is not valid");
     }
 
     switch (op) {
